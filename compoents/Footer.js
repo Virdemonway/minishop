2ff39978f001e3144098887b07d2cb0610bccecd
Virdemonway <73302606+Virdemonway@users.noreply.github.com>
2025-05-21 11:17:11 +0800

    Delete compoents directory

--- compoents/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 bg-opacity-80 p-4 text-center">
-      <p className="text-white opacity-80">© 2025 Art & Design Ecommerce</p>
-    </footer>
-  );
-}
