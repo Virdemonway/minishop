1518fd8cc53e066b49029c53a5b61dfc5f158194
Virdemonway <73302606+Virdemonway@users.noreply.github.com>
2025-05-21 11:12:33 +0800

    Update Footer.js

---
+++ compoents/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 bg-opacity-80 p-4 text-center">
+      <p className="text-white opacity-80">© 2025 Art & Design Ecommerce</p>
+    </footer>
+  );
+}
